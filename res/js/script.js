a8f896a336775d9f3ce27c6e3c3c23f7f40d0aca
Maas Lalani <maas@live.ca>
2016-07-30 22:41:43 -0400

    Cleaned files and code

--- res/js/script.js
+++ res/js/script.js
@@ -1,8 +1,10 @@
 $(function() {
-	setTimeout(function() {
-		$('.hexanote').removeClass('hidden');
-	}, 500);
+  
+  setTimeout(function() {
+    $('.hexanote').removeClass('hidden');
+  }, 500);
 
-	$('#lead').delay(3000).animate({marginTop: '-=250px'}, {duration: 750});
-	$('.hexagon').delay(3500).animate({opacity: '1'}, {duration: 750});
+  $('#lead').delay(3000).animate({marginTop: '-=250px'}, {duration: 750});
+  $('.hexagon').delay(3500).animate({opacity: '1'}, {duration: 750});
+
 })();
