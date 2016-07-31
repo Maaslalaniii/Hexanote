e7f64cf90d2705520274c87dbb8beb0028802594
Maas Lalani <maas@live.ca>
2016-07-30 23:40:55 -0400

    Added animation

--- res/js/script.js
+++ res/js/script.js
@@ -2,9 +2,8 @@ $(function() {
   
   setTimeout(function() {
     $('.hexanote').removeClass('hidden');
+    $('#lead').delay(3000).animate({marginTop: '-=225px'}, {duration: 750});
+    $('.hexagon').delay(3500).animate({opacity: '1'}, {duration: 750});
   }, 500);
-
-  $('#lead').delay(3000).animate({marginTop: '-=250px'}, {duration: 750});
-  $('.hexagon').delay(3500).animate({opacity: '1'}, {duration: 750});
 
 })();
