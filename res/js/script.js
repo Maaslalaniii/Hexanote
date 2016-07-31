fcd625cdb81ce81e67dffddd67f7c0d0d9b18dcd
Maas Lalani <maas@live.ca>
2016-07-31 11:44:05 -0400

    Fixed e on seperate line issue

--- res/js/script.js
+++ res/js/script.js
@@ -1,9 +1,9 @@
 $(function() {
-  
+
   setTimeout(function() {
     $('.hexanote').removeClass('hidden');
     $('#lead').delay(3000).animate({marginTop: '-=225px'}, {duration: 750});
     $('.hexagon').delay(3500).animate({opacity: '1'}, {duration: 750});
   }, 500);
-
+  
 })();
