a443e4737740b83d620c5bf92140b490e35ed471
Maas Lalani <maas@live.ca>
2016-07-31 22:59:09 -0400

    Added subject buttons and hexagon auto-rotate

--- res/js/script.js
+++ res/js/script.js
@@ -1,33 +1,23 @@
 $(function() {
 
-  var img = $('.hexagon');
-  var offset = img.offset();
+  var hexagon = $('.hexagon');
+  var hexanote = $('.hexanote');
+  var lead = $('#lead');
+  var subjectList = $('.subject-list');
 
-  $('.hexanote').delay(500).queue(function(){
-    $(this).removeClass('hidden');
-    $(this).dequeue();
+
+  hexanote.delay(500).queue(function(){
+    hexanote.removeClass('hidden');
+    hexanote.dequeue();
   });
-  $('#lead').delay(3000).animate({marginTop: '-=225px'}, {duration: 750});
-  img.delay(4000).animate({opacity: '1'}, {duration: 750});
 
-  $(document).mousemove(function(e){
-    var center_x = (offset.left) + (img.width()/2);
-    var center_y = (offset.top) + (img.height()/2);
-    var mouse_x = event.pageX; var mouse_y = event.pageY;
-    var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
-    var degree = (radians * (180 / Math.PI) * -1) + 90;
+  lead.delay(3000).animate({marginTop: '-=225px'}, {duration: 750});
+  hexagon.delay(4000).animate({opacity: '1'}, {duration: 750});
+  subjectList.delay(5000).animate({opacity: '1'}, {duration: 750});
 
-    img.css('-moz-transform', 'rotate('+degree+'deg)');
-    img.css('-webkit-transform', 'rotate('+degree+'deg)');
-    img.css('-o-transform', 'rotate('+degree+'deg)');
-    img.css('-ms-transform', 'rotate('+degree+'deg)');
-    img.css('-webkit-filter', 'hue-rotate('+degree+'deg)')
-
-    var $width = ($(document).width())/255;
-    var $height = ($(document).height())/255;
-    var $pageX = parseInt(e.pageX / $width,10);
-    var $pageY = parseInt(e.pageY / $height,10);
-    $('.polygon').css("stroke", "rgb("+$pageX+","+$pageY+","+$pageX+")");
-  }); 
 
 });
+
+function hexColor(color) {
+  $('.polygon').css("stroke", "#"+color);
+}
