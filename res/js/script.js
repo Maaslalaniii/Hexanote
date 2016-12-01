432331240f875e73e6416ad1f2d93158392f1c43
Maas Lalani <maas@live.ca>
2016-12-01 16:50:58 -0500

    Removed unnecessary whitespace

--- res/js/script.js
+++ res/js/script.js
@@ -1,21 +1,17 @@
 $(function() {
-
   $(".hexanote").delay(500).queue(function(){
     $(this).removeClass("hidden");
     $(this).dequeue();
     hexColor('1abc9c', 'english');
   });
 
-  $("#lead")        .delay(3000).animate({ marginTop: "-=225px"}, { duration: 750 });
-  $(".hexagon")     .delay(4000).animate({   opacity: "1"},       { duration: 750 });
-  $(".subject-list").delay(4000).animate({   opacity: "1"},       { duration: 750 });
-
+  $("#lead").delay(3000).animate({ marginTop: "-=225px"}, { duration: 750 });
+  $(".hexagon").delay(4000).animate({ opacity: "1"}, { duration: 750 });
+  $(".subject-list").delay(4000).animate({ opacity: "1"}, { duration: 750 });
 });
 
 function hexColor(color, subject) {
-
-  $(".subject") .css("color",  "#fff");
-  $("#"+subject).css("color",  "#"+color);
-  $(".polygon") .css("stroke", "#"+color);
-
+  $(".subject").css("color", "#fff");
+  $("#"+subject).css("color", "#"+color);
+  $(".polygon").css("stroke", "#"+color);
 }
